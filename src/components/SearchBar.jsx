f2a43f1be193f5471381fa6e3d43c7edb7e5e7ec
AdityaKushwaha <miaditya.kushwaha@gmail.com>
2024-07-25 21:03:06 +0530

    SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,5 +1,17 @@
-export default function SearchBar () {
+export default function SearchBar({ handleChange }) {
+
+    const debounce = () => {
+        let timer;
+        return (
+            (e) => {
+                timer && clearTimeout(timer);
+                timer = setTimeout(() => handleChange('title', e.target.value), 1000);
+            }
+        )
+    }
+    const debouncedChangeHandler = debounce();
+
     return (
-        <input type='text'/>
+        <input type='text' onChange={debouncedChangeHandler} />
     )
 }
--- src/molecules/ControlBar.js
+++ src/molecules/ControlBar.js
@@ -6,7 +6,6 @@ export default function ControlBar() {
     let [searchParams, setSearchParams] = useSearchParams();
 
     const handleChange = (key, value = null) => {
-
         setSearchParams(params => {
             const {page, ...paramsObj} = Object.fromEntries(params);
 
@@ -17,7 +16,7 @@ export default function ControlBar() {
                 const { [key]: discardValue, page, ...newParams } = paramsObj;
                 return newParams;
             }
-        })
+        });
     }
 
     return (
